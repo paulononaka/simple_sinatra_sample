87bb6af8e00f4a6d2c2ff71167651cf9afae80fd
Paulo Henrique Nonaka <paulononaka@gmail.com>
2013-06-22 16:04:45 -0300

    initial layout

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source :rubygems
 
 gem 'sinatra'
 gem 'rake'
+gem 'shotgun'
+gem 'haml'
 
 group :development, :test do
   gem 'pry'
--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ $LOAD_PATH << './lib'
 require 'rubygems'
 require 'sinatra'
 
+configure do
+  enable :sessions
+
+  set :public_folder, Proc.new { File.join(root, "static") }
+end
+
 get '/' do
-  'hello world'
+  haml :index 
 end
